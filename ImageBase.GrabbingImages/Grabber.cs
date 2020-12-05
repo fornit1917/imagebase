a93175ec2227ac4fd146d7c4d56e6bafe687bfc7
Дмитрий Богданов <bogdanov_s69@mail.ru>
2020-12-05 18:52:39 +0300

    #27 Add Project for grabbing free iamge from Pexels

---
+++ ImageBase.GrabbingImages/Grabber.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using PexelsDotNetSDK.Api;
+using PexelsDotNetSDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageBase.GrabbingImages
+{
+    public class Grabber
+    {
+        public IConfiguration Configuration { get; }
+        public Grabber(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            pexelsClient = new PexelsClient(Configuration.GetConnectionString("MyAPIKey"));
+        }
+        private PexelsClient pexelsClient;
+
+        public async Task<PhotoPage> SearchPhotosAsync()
+        {
+            PhotoPage photoPage = await pexelsClient.SearchPhotosAsync("Nature","ru-RU",1,5);
+            return photoPage;
+        }
+    }
+}
---
+++ ImageBase.GrabbingImages/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using PexelsDotNetSDK.Models;
+using System;
+
+namespace ImageBase.GrabbingImages
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+             .AddUserSecrets(typeof(Program).Assembly)
+             .Build();
+            Grabber grabber = new Grabber(config);
+            PhotoPage photoPage = await grabber.SearchPhotosAsync();
+        }
+    }
+}
